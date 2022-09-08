34ac9560053927e99e1f3714db716a1e2d166a48
AnaZhi <zhilin.anatoly@gmail.com>
2022-09-08 18:16:01 +0300

    generation of file numbers and names added

--- main.ps1
+++ main.ps1
@@ -1,18 +1,21 @@
 
+#Var definition
 $ini = Get-Content -Path .\folder_ini.ini 
 
 $section = "none" #section name in ini file
 $projectName = "no project name"
-$ProjectNumber = "no project number"
-$ProjectCode = "no project code"
-$Author = "no author"
-$Manager = "no manager"
-$Company = "no company"
+[int]$projectNumber = 000
+$projectCode = "no project code"
+$projectEngineer = "no engineer"
+$projectManager = "no manager"
+$projectCompany = "no company"
 
-$Site = @()
-$Document = @()
-$iniHash = @{}
-$iniTemp = @()
+$projectSites = @() # list of project site
+$ProjectDocuments = @() # list of hash tables of Project Documents Properties
+#$ProjectDocuments = @( @{ DocumentName = ; DocumentQt = ; DocumentPath = } )
+
+$ProjectItems = @() # list of hash tables of Project items
+#ProjectItems =  @( @{ ItemNumber = ; ItemTitle = ; ItemSite = ; ItemFileName =  })
 
 #Ini File parser
 foreach ($line in $ini)
@@ -30,20 +33,20 @@ foreach ($line in $ini)
                     if ($SpliteArray[0].trim('') -eq "ProjectName") {$projectName = $SpliteArray[1].trim('')}
                     if ($SpliteArray[0].trim('') -eq "ProjectNumber") {$ProjectNumber = $SpliteArray[1].trim('')}
                     if ($SpliteArray[0].trim('') -eq "ProjectCode") {$ProjectCode = $SpliteArray[1].trim('')}
-                    if ($SpliteArray[0].trim('') -eq "Author") {$Author = $SpliteArray[1].trim('')}
-                    if ($SpliteArray[0].trim('') -eq "Manager") {$Manager = $SpliteArray[1].trim('')}
-                    if ($SpliteArray[0].trim('') -eq "Company") {$Company = $SpliteArray[1].trim('')}
+                    if ($SpliteArray[0].trim('') -eq "ProjectEngineer") {$projectEngineer = $SpliteArray[1].trim('')}
+                    if ($SpliteArray[0].trim('') -eq "projectManager") {$projectManager = $SpliteArray[1].trim('')}
+                    if ($SpliteArray[0].trim('') -eq "projectCompany") {$projectCompany = $SpliteArray[1].trim('')}
                 }
-# Section Site
+# Section ProjectSites
                 if ($section -eq 'Site')
                 {
-                    $Site += $Line.Trim('')
+                    $ProjectSites += $Line.Trim('')
                 }
-# Section Document
+# Section ProjectDocuments
                 if ($section -eq 'Document')
                 {
                     $SpliteArray = $Line.Split(",")
-                    $Document += @{DocumentName=$SpliteArray[0].trim(''); DocumentQt=$SpliteArray[1].trim(''); DocumentPath=$SpliteArray[2].trim('')}
+                    $ProjectDocuments += @{ DocumentName = $SpliteArray[0].trim(''); DocumentQt = $SpliteArray[1].trim(''); DocumentPath = $SpliteArray[2].trim('')}
                 }                
 
             }
@@ -54,9 +57,52 @@ foreach ($line in $ini)
     "Project Name is $projectName"
     "Project Code is $ProjectCode"
     "Project Number is $ProjectNumber"    
-    "Project Author is $Author"
-    "Project Manager is $Manager"
-    "Project Company is $Company"
-    $site
-    $Document
+    "Project Engineer is $ProjectEngineer"
+    "Project Manager is $projectManager"
+    "Project Company is $projectCompany"
+   for ($i = 0; $i -lt $projectSites.Length; $i++) {"Project Site $($i + 1) is $($ProjectSites[$i])"} 
+"Number of Sites is $($projectSites.Count)"
+# Calculation Qty of Items
+$DocumentQty = 0
+foreach ($Doc in $ProjectDocuments)
+    { 
+        if ($Doc.DocumentQt -like "single") { $DocumentQty += 1 }
+        if ($Doc.DocumentQt -like "multi") { $DocumentQty += $($ProjectSites.Count) }
+    }
+"Number of Documents is $DocumentQty"
+#$ProjectDocuments
+
+$ItemSmallNumber = 1
+foreach ($Doc in $ProjectDocuments)
+    {
+        if ($Doc.DocumentQt -like "single")
+        { 
+            [string]$tempNumber = ($projectNumber * 1000) + $ItemSmallNumber
+            $tempNumber += "-01"
+
+            [string]$tempFileName = $projectName.Replace(' ', '_') + '_' + $Doc.DocumentName.Replace(' ', '_') + '_' + $tempNumber
+
+            $ProjectItems += @{ItemNumber = $tempNumber; ItemTitle = $Doc.DocumentName; ItemSite = "General"; ItemFileName = $tempFileName}
+            $ItemSmallNumber++
+        }
+        if ($Doc.DocumentQt -like "multi")
+        {
+            
+            foreach ($site in $projectSites)
+            {
+                [string]$tempNumber = ($projectNumber * 1000) + $ItemSmallNumber
+                $tempNumber += "-01"
+                
+                [string]$tempFileName = $Site.Replace(' ', '_') + '_' + $Doc.DocumentName.Replace(' ', '_') + '_' + $tempNumber
+                
+                $ProjectItems += @{ItemNumber = $tempNumber; ItemTitle = $Doc.DocumentName; ItemSite = $site; ItemFileName = $tempFileName}
+                $ItemSmallNumber++
+            }
+
+            
+
+        }
+    }
+"Project Items"    
+$ProjectItems[0..$($ProjectItems.Length - 1)]
 #>
--- word.ps1
+++ word.ps1
@@ -2,16 +2,14 @@ $Word = New-Object -ComObject Word.application
 $Word.visible = $true
 $WordDocument = $Word.Documents.add()
 $Selection = $Word.Selection
+$WordRange = $Selection.range
 $WordSection = $WordDocument.Sections.Item(1)
 
 #Landscape orientation
 $WordDocument.PageSetup.Orientation = 1
 
 #Header
-
 $Header = $WordSection.Headers.Item(1)
-$Header.ParagraphFormat.Alignment = 0
-$Header.Font.Size = 8
 $Header.range.text = "header text"
 
 #Footer
@@ -23,8 +21,8 @@ $Selection.ParagraphFormat.Alignment = 1
 $Selection.Font.Bold = 1
 $Selection.Font.Size = 18
 $Selection.TypeText("Documentation List")
+
 #Description
-$Selection.TypeParagraph()
 $Selection.TypeParagraph()
 $Selection.ParagraphFormat.Alignment = 0
 $Selection.Font.Bold = 0
@@ -36,3 +34,37 @@ $Selection.TypeParagraph()
 $Selection.TypeText("Project Manager: Name")
 $Selection.TypeParagraph()
 $Selection.TypeText("Project Manager: Name")
+
+#Table name Design Documentation
+$Selection.TypeParagraph()
+$Selection.ParagraphFormat.Alignment = 0
+$Selection.Font.Bold = 1
+$Selection.Font.Size = 18
+$Selection.TypeText("Design Documentation")
+$Selection.TypeParagraph()
+
+#Table
+$Selection.Font.Bold = 0
+$Selection.Font.Size = 11
+$Selection.ParagraphFormat.Alignment = 1
+# $selection.Shading.BackgroundPatternColorIndex = 16
+
+$table = $Selection.tables.add($selection.range,2,5) #Change number of Rows
+$table.Borders.InsideLineStyle = 1
+$table.Borders.OutsideLineStyle = 1
+
+#First Raw
+$table.Rows.Item(1).Shading.ForegroundPatternColorIndex = 16
+
+$table.cell(1,1).range.text = "Doc No"
+$table.cell(1,2).range.text = "Document name"
+$table.cell(1,3).range.text = "Date"
+$table.cell(1,4).range.text = "Creator"
+$table.cell(1,5).range.text = "Note"
+#Other Rows
+
+$table.cell(2,1).range.text = "1 Doc No"
+$table.cell(2,2).range.text = "1 Document name"
+$table.cell(3,3).range.text = "1 Date"
+$table.cell(4,4).range.text = "1 Creator"
+$table.cell(5,5).range.text = "1 Note"
